8b7209ead72be28faed29f258bad47f9e17c4d47
Eduardo da Silva <eduardo.silva@gmail.com>
2023-02-28 10:48:59 -0300

    OnLoad JS

--- assets/scripts/copyCode.js
+++ assets/scripts/copyCode.js
@@ -1,28 +1,31 @@
 // This assumes that you're using Rouge; if not, update the selector
-const codeBlocks = document.querySelectorAll(
-  '.code-header + .highlighter-rouge',
-);
-const copyCodeButtons = document.querySelectorAll('.copy-code-button');
 
-copyCodeButtons.forEach((copyCodeButton, index) => {
-  const code = codeBlocks[index].innerText;
+window.onload = () => {
+  const codeBlocks = document.querySelectorAll(
+    '.code-header + .highlighter-rouge',
+  );
+  const copyCodeButtons = document.querySelectorAll('.copy-code-button');
 
-  copyCodeButton.addEventListener('click', () => {
-    // Copy the code to the user's clipboard
-    window.navigator.clipboard.writeText(code);
-    alert('Todo: Copy code to clipboard');
+  copyCodeButtons.forEach((copyCodeButton, index) => {
+    const code = codeBlocks[index].innerText;
 
-    // Update the button text visually
-    const { innerText: originalText } = copyCodeButton;
-    copyCodeButton.innerText = 'Copied!';
+    copyCodeButton.addEventListener('click', () => {
+      // Copy the code to the user's clipboard
+      window.navigator.clipboard.writeText(code);
+      alert('Todo: Copy code to clipboard');
 
-    // (Optional) Toggle a class for styling the button
-    copyCodeButton.classList.add('copied');
+      // Update the button text visually
+      const { innerText: originalText } = copyCodeButton;
+      copyCodeButton.innerText = 'Copied!';
 
-    // After 2 seconds, reset the button to its initial UI
-    setTimeout(() => {
-      copyCodeButton.innerText = originalText;
-      copyCodeButton.classList.remove('copied');
-    }, 2000);
+      // (Optional) Toggle a class for styling the button
+      copyCodeButton.classList.add('copied');
+
+      // After 2 seconds, reset the button to its initial UI
+      setTimeout(() => {
+        copyCodeButton.innerText = originalText;
+        copyCodeButton.classList.remove('copied');
+      }, 2000);
+    });
   });
-});
+};
